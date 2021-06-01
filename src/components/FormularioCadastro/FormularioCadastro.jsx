6e44d75698be3600550a8cf2a311fd39f4208879
Felipe Silva <felipe.dacs@gmail.com>
2021-06-01 01:36:21 -0300

    :tv: add aula 3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,27 @@
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 export function FormularioCadastro() {
   return (
     <form>
-      <label>Nome</label>
-      <input type="text"/>
-
-      <label>Sobrenome</label>
-      <input type="text"/>
-
-      <label>CPF</label>
-      <input type="text"/>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+      />
 
       <label>Promoções</label>
       <input type="checkbox"/>
