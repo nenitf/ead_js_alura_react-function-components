bd427a3883a33e5f4520f5035de88eb0e8c7f7d4
Felipe Silva <felipe.dacs@gmail.com>
2021-06-01 01:26:12 -0300

    :tv: add aula 2

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,7 +1,30 @@
+import Button from '@material-ui/core/Button'
+
 export function FormularioCadastro() {
   return (
     <form>
+      <label>Nome</label>
       <input type="text"/>
+
+      <label>Sobrenome</label>
+      <input type="text"/>
+
+      <label>CPF</label>
+      <input type="text"/>
+
+      <label>Promoções</label>
+      <input type="checkbox"/>
+
+      <label>Novidades</label>
+      <input type="checkbox"/>
+
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+      >
+        Cadastrar
+      </Button>
     </form>
   )
 }
